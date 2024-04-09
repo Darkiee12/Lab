efdf6af00dfbda629e7e74f1dbc54c35330c3295
Darkness <kizluxury0506@gmail.com>
2024-04-09 19:50:58 +0700

    finish lab3

--- Lab3/src/main/java/lab3/controllers/RegistrationController.java
+++ Lab3/src/main/java/lab3/controllers/RegistrationController.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.mail.*;
+import jakarta.mail.internet.*;
 import lab3.models.Major;
 import lab3.models.Programme;
 import lab3.models.Course;
@@ -101,7 +104,43 @@ public class RegistrationController {
     request.setAttribute("email", email);
     request.setAttribute("major", major);
     request.setAttribute("courses", courses);
+    // sendEmail(name, id, email, major, courses);
     return "forward:/WEB-INF/views/ResultRegister.jsp";
   }
+
+  private void sendEmail(String name, String id, String email, String major, List<String> courses) {
+    StringBuilder messageBuilder = new StringBuilder();
+    messageBuilder.append("Thank you for registering!\n\n")
+    .append("Name: ").append(name).append("\n")
+    .append("ID: ").append(id).append("\n")
+   .append("Email: ").append(email).append("\n")
+    .append("Major: ").append(major).append("\n")
+    .append("Courses: ").append(String.join(", ", courses)).append("\n");
+
+    try {
+        String host = "localhost";
+        String from = "lab3@localhost";
+        int port = 8080;
+        String to = email;
+
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+        properties.setProperty("mail.smtp.port", String.valueOf(port));
+
+        Session session = Session.getDefaultInstance(properties);
+
+        MimeMessage message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(from));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+        message.setSubject("Registration Confirmation");
+        message.setText(messageBuilder.toString());
+
+        Transport.send(message);
+        System.out.println("Email sent successfully.");
+    } catch (MessagingException mex) {
+        mex.printStackTrace();
+        
+    }
+}
 
 }
